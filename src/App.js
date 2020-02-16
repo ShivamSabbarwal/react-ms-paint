a4fd3016bc992df4ed03ba54bb48da0aac27da4e
shivamsabbarwal <shivam.sabb@gmail.com>
2020-02-16 17:20:32 -0400

    adding tools svg

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React, { Component, Fragment } from "react";
+import React, { Component } from "react";
 
 // Component Imports
-import ColorPicker from "../src/components/ColorPicker";
+import ColorPicker from "./components/ColorPicker";
+import Canvas from "./components/Canvas";
 
 // Image Imports
-import pencil from "../src/images/pencil.svg";
-import fill from "../src/images/fill.svg";
-import picker from "../src/images/picker.svg";
+import pencil from "./images/pencil.svg";
+import fill from "./images/fill.svg";
+import picker from "./images/picker.svg";
 
 // Styling Imports
 import "./App.css";
@@ -14,19 +15,19 @@ import "./App.css";
 const defaultColor = "black";
 const defaultTool = "Pencil";
 
-// const toolbarItems = [
-//   { name: "Pencil", image: pencil },
-//   { name: "Fill", image: fill },
-//   { name: "Picker", image: picker }
-// ];
+const toolbarItems = [
+  { name: "Pencil", image: pencil },
+  { name: "Fill", image: fill },
+  { name: "Picker", image: picker }
+];
 
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       color: defaultColor,
-      selectedItem: defaultTool
-      // toolbarItems: toolbarItems
+      selectedItem: defaultTool,
+      toolbarItems: toolbarItems
     };
 
     this.changeColor = this.changeColor.bind(this);
@@ -43,11 +44,15 @@ export default class App extends Component {
 
   render() {
     return (
-      <Fragment className="App">
-        <header className="App-header">
-          <ColorPicker />
-        </header>
-      </Fragment>
+      <div className="App">
+        <Canvas
+          items={this.state.toolbarItems}
+          activeItem={this.state.selectedItem}
+          handleClick={this.changeTool}
+          color={this.state.color}
+        />
+        <ColorPicker />
+      </div>
     );
   }
 }
--- src/components/ColorPicker.js
+++ src/components/ColorPicker.js
@@ -1,9 +1,40 @@
 import React, { Component } from "react";
 import { SketchPicker } from "react-color";
 
+const palette = [
+  "#000000",
+  "#787878",
+  "#790300",
+  "#757A01",
+  "#007902",
+  "#007778",
+  "#0A0078",
+  "#7B0077",
+  "#767A38",
+  "#003637",
+  "#286FFE",
+  "#083178",
+  "#4C00FE",
+  "#783B00",
+  "#FFFFFF",
+  "#BBBBBB",
+  "#FF0E00",
+  "#FAFF08",
+  "#00FF0B",
+  "#00FEFF",
+  "#3400FE",
+  "#FF00FE",
+  "#FBFF7A",
+  "#00FF7B",
+  "#76FEFF",
+  "#8270FE",
+  "#FF0677",
+  "#FF7D36"
+];
+
 class ColorPicker extends Component {
   render() {
-    return <SketchPicker />;
+    return <SketchPicker presetColors={palette} width="360px" />;
   }
 }
 
